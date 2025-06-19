09d9b3333a06e4785a72c78f882b3eac79fffec8
Petr Kozorezov <petr.kozorezov@gmail.com>
2025-06-19 14:06:50 +0300

    simplify a little bit

--- README.md
+++ README.md
@@ -13,7 +13,7 @@ This is an example of using [Elixir/Nx](https://github.com/elixir-nx/) with CUDA
 
 1. [install Nix and Devenv](https://devenv.sh/getting-started/)
 2. enter the dev environment `devenv shell` -- this will take a time to download all needed packages including CUDA libraries
-3. start livebook locally `devenv up`
+3. start livebook locally `livebook start`
 4. open the livebook link shown in the step above
 5. open and run `test.livemd` notebook
 6. if everything is OK you will see Nx tensor on cuda backend like this
--- devenv.nix
+++ devenv.nix
@@ -1,7 +1,6 @@
 { config, lib, pkgs, ... }: let
   cudaPackages = pkgs.cudaPackages;
   cudaPath     = cudaPackages.cudatoolkit;
-  nvidiaDriver = pkgs.linuxPackages.nvidia_x11;
 in {
   packages = with pkgs; [
     gcc
@@ -12,8 +11,6 @@ in {
   languages.elixir.enable = true;
 
   env = {
-    NIX_LD = "${pkgs.glibc}/lib/ld-linux-x86-64.so.2";
-
     XLA_TARGET = "cuda12";
     XLA_FLAGS  = "--xla_gpu_cuda_data_dir='${cudaPath}'";
     CUDA_PATH  = cudaPath;
@@ -22,17 +19,12 @@ in {
     LIVEBOOK_DATA_PATH = "${config.devenv.root}/data/livebook";
   };
 
-  processes.livebook.exec = "livebook start";
-  process.managers.process-compose.tui.enable = false;
-
   enterShell = ''
     export LD_LIBRARY_PATH="/run/opengl-driver/lib:$LD_LIBRARY_PATH" # for NixOS
     export LD_LIBRARY_PATH="${cudaPath}/lib:$LD_LIBRARY_PATH"
-    export LD_LIBRARY_PATH="${cudaPackages.cudatoolkit}/lib:$LD_LIBRARY_PATH"
     export LD_LIBRARY_PATH="${cudaPackages.cudnn.lib}/lib:$LD_LIBRARY_PATH"
     export LD_LIBRARY_PATH="${cudaPackages.nccl}/lib:$LD_LIBRARY_PATH"
     export LD_LIBRARY_PATH="${cudaPackages.libnvjitlink.lib}/lib:$LD_LIBRARY_PATH"
-    # echo LD_LIBRARY_PATH=$LD_LIBRARY_PATH
 
     elixir ${./cuda-xla-test.exs} || exit 1
   '';
